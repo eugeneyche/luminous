9919eef39995fd3009500cb432be7e56b53ec1eb
Eugene Che <eugeneyche@gmail.com>
2015-07-11 16:13:24 -0700

    restarted lighthouse

--- init.lua
+++ init.lua
@@ -2,5 +2,6 @@ return {
     widget = {
 		hello = require("luminous.widget.hello"),
 		custom_draw = require("luminous.widget.custom_draw"),
+		lighthouse = require("luminous.widget.lighthouse")
     }
 }
